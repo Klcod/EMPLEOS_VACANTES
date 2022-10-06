bf4bb54457eaf4379378084080ecd9a3958cc320
Celeste_Chan <kowng26@gmail.com>
2022-10-06 12:19:44 -0500

    Implementación de las dependencias al pom
    Pase las nuevas propiedades al archivo .properties y transferí las nuevas clases de modelo, creamos el paquete .repository con nuevas clases y por ultimo cree el nuevo paquete CategoriasServiceJpa dentro del paquete service el cual tendrá la implementación de los métodos guardar, buscarTodas y buscarPorId

---
+++ src/main/java/net/itinajero/service/db/CategoriasServiceJpa.java
@@ -0,0 +1,39 @@
+package net.itinajero.service.db;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.itinajero.model.Categoria;
+import net.itinajero.repository.CategoriasRepository;
+import net.itinajero.service.ICategoriasService;
+
+@Service
+public class CategoriasServiceJpa implements ICategoriasService {
+	
+	@Autowired
+	private CategoriasRepository categoriasRepo;
+
+	@Override
+	public void guardar(Categoria categoria) {
+		categoriasRepo.save(categoria);
+
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		return categoriasRepo.findAll();
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		Optional<Categoria> optional = categoriasRepo.findById(idCategoria);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+
+}
