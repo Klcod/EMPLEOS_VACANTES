14ab18d1fa013f12548dad84cfeeeeabe5c14556
Celeste_Chan <kowng26@gmail.com>
2022-10-06 17:10:33 -0500

    Utilice la anotación @Primary para solucionar el arranque en la consola
    Modifique las clases CategoriasServiceJpa, CategoriasController, CategoriasServicempl, se utilizo la anotación @Qualifier en las clases CategoriasController y en VacantesController pero luego las modifique por @Anotación, se creo VacantesServiceJpa en el paquete .service.db y modifique listVacantes

--- src/main/java/net/itinajero/controller/CategoriasController.java
+++ src/main/java/net/itinajero/controller/CategoriasController.java
@@ -3,6 +3,7 @@ package net.itinajero.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,8 +19,9 @@ import net.itinajero.service.ICategoriasService;
 @RequestMapping(value="/categorias")
 public class CategoriasController {
 	
-	
+
 	@Autowired
+	//@Qualifier("categoriasServiceJpa")
    	private ICategoriasService serviceCategorias;
 	
 	@RequestMapping(value="/index", method=RequestMethod.GET)
--- src/main/java/net/itinajero/service/db/CategoriasServiceJpa.java
+++ src/main/java/net/itinajero/service/db/CategoriasServiceJpa.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import net.itinajero.model.Categoria;
@@ -11,23 +12,24 @@ import net.itinajero.repository.CategoriasRepository;
 import net.itinajero.service.ICategoriasService;
 
 @Service
+@Primary
 public class CategoriasServiceJpa implements ICategoriasService {
 	
 	@Autowired
 	private CategoriasRepository categoriasRepo;
 
-	@Override
+	
 	public void guardar(Categoria categoria) {
 		categoriasRepo.save(categoria);
 
 	}
 
-	@Override
+	
 	public List<Categoria> buscarTodas() {
 		return categoriasRepo.findAll();
 	}
 
-	@Override
+	
 	public Categoria buscarPorId(Integer idCategoria) {
 		Optional<Categoria> optional = categoriasRepo.findById(idCategoria);
 		if (optional.isPresent()) {
